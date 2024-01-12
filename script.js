3cc9fb55c514860d58a1b3ad7b9a0c63c3c404c1
Eleskerli Lale <eleskerlilale@gmail.com>
2024-01-12 13:06:24 +0400

    water game

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const cup=document.querySelectorAll(".cup")
+const remained=document.querySelector(".remained")
+const cup=document.querySelectorAll(".cup-small")
 const liters=document.querySelector(".liters")
 const percentage=document.querySelector(".percentage")
-
 const arr=[]
-console.log(cup);
-
+cup.forEach(e => {
+    arr.push(e)
+})
 cup.forEach(element => {
     element.addEventListener("click" , () => {
-        console.log(cup)
-        element.classList.add("full")
-
+        let height=0;
+        const id = arr.indexOf(element)
+        height=height+37.5*(id+1)
+        percentage.style.height=`${height}px`
+        percentage.innerText=`${12.5*(id+1)}`
+        cup.forEach((elem, i) => {
+            if(i<=id){
+                cup[i].classList.add("full")
+                remained.style.display='flex'
+            }else{
+                remained.style.display='flex'
+                cup[i].classList.remove("full")
+            }if(id==7){
+                // remained.style.display='none'
+            }
+        })
     })
 })
